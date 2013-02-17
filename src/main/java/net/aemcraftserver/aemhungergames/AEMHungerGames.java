a56ae96ea4a32348117654c3799582e2d7e4b13d
Dan <theexplosivefrog@aim.com>
2013-02-17 15:15:28 -0500

    Test commit

--- src/main/java/net/aemcraftserver/aemhungergames/AEMHungerGames.java
+++ src/main/java/net/aemcraftserver/aemhungergames/AEMHungerGames.java
@@ -1,6 +1,18 @@
 package main.java.net.aemcraftserver.aemhungergames;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AEMHungerGames extends JavaPlugin{
+	Logger log;
+	
+	@Override
+	public void onEnable(){
+		log.log(Level.INFO, "[AEMHungerGames] Enabled.");
+	}
+	public void onDisable(){
+		log.log(Level.INFO, "[AEMHungerGames] Disabled.");
+	}
 }
