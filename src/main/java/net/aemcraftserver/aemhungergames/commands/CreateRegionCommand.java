9148cd37e5828eba8c1dc8b40b0d3af316079a37
th3controller <th3controller2000@gmail.com>
2013-02-18 15:07:36 -0500

    Added a new finished region creator.

---
+++ src/main/java/net/aemcraftserver/aemhungergames/commands/CreateRegionCommand.java
@@ -0,0 +1,32 @@
+package main.java.net.aemcraftserver.aemhungergames.commands;
+
+import main.java.net.aemcraftserver.aemhungergames.util.RegionConfig;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class CreateRegionCommand implements CommandExecutor {
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		if(args.length == 3) {
+			Player player = ((Player)sender);
+			if(args[0].equalsIgnoreCase("create") && player.hasPermission("aemhg.createregion")) {
+				if(args[2].equalsIgnoreCase("pos1")) {
+					RegionConfig.regions.set("regions." + player.getWorld().getName() + args[1] + ".P1", player.getLocation().toString());
+				}
+				else if(args[2].equalsIgnoreCase("pos2")) {
+					RegionConfig.regions.set("regions." + player.getWorld().getName() + args[1] + ".P2", player.getLocation().toString());
+				} else {
+					sender.sendMessage("Incorrect arguments");
+				}
+			} else {
+				sender.sendMessage("Incorrect arguments");
+			}
+		} else {
+			sender.sendMessage("Incorrect arguments");
+		}
+		return true;
+	}
+}
---
+++ src/main/java/net/aemcraftserver/aemhungergames/listeners/BlockListener.java
@@ -0,0 +1,73 @@
+package main.java.net.aemcraftserver.aemhungergames.listeners;
+
+import java.util.Set;
+
+import main.java.net.aemcraftserver.aemhungergames.AEMHungerGames;
+import main.java.net.aemcraftserver.aemhungergames.util.RegionConfig;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockPlaceEvent;
+
+public class BlockListener implements Listener {
+	
+	AEMHungerGames plugin;
+	
+	public BlockListener(AEMHungerGames plugin) {
+		this.plugin = plugin;
+	}
+	@EventHandler
+	public void BlockPlace(BlockPlaceEvent event) {
+		Player player = event.getPlayer();
+		Location loc = event.getBlock().getLocation();
+		Set<String> regions = RegionConfig.regions.getConfigurationSection("Regions").getKeys(false);
+		for(String region : regions) {
+			Location p1 = toLocation(RegionConfig.regions.getString("regions." + event.getBlock().getWorld().getName() + region + ".P1"));
+			Location p2 = toLocation(RegionConfig.regions.getString("regions." + event.getBlock().getWorld().getName() + region + ".P2"));
+			
+			double maxX = (p1.getX() > p2.getX()) ? p1.getX() : p2.getX();
+			double minX = (p1.getX() < p2.getX()) ? p1.getX() : p2.getX();
+			if(loc.getX() <= maxX && loc.getX() >= minX) {
+				double maxY = (p1.getY() > p2.getY()) ? p1.getY() : p2.getY();
+				double minY = (p1.getY() < p2.getY()) ? p1.getY() : p2.getY();
+				if(loc.getY() <= maxY && loc.getY() >= minY) {
+					double maxZ = (p1.getZ() > p2.getZ()) ? p1.getZ() : p2.getZ();
+					double minZ = (p1.getZ() < p2.getZ()) ? p1.getZ() : p2.getZ();
+					if(loc.getZ() <= maxZ && loc.getZ() >= minZ) {
+						if(player.hasPermission("aemhg.placeblock") || player.isOp()) {
+							event.setCancelled(true);
+							cm(player, "&c - Invalid action, you are inside a protected zone");
+						}
+					}
+					continue;
+				}
+				continue;
+			}
+			continue;
+		}
+	}
+	public void cm(Player player, String string) {
+		player.sendMessage(ChatColor.translateAlternateColorCodes('&', string));
+	}
+	public Location toLocation(String location) {
+		String[] loc = location.split(" ");
+		try
+		{	
+			World world = Bukkit.getServer().getWorld(loc[0]);
+			int x = Integer.parseInt(loc[1]);
+			int y = Integer.parseInt(loc[2]);
+			int z = Integer.parseInt(loc[3]);
+			return new Location(world, x, y, z);
+		}
+		catch(NumberFormatException err)
+		{
+			// Something went wrong!
+			return null;
+		}
+	}
+}
