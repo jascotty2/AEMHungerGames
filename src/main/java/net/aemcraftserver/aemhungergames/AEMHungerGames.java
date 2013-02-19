d38790de72e13f3d538aae921322426eedca3e87
Dan <theexplosivefrog@aim.com>
2013-02-18 23:46:49 -0500

    Oops.

--- src/main/java/net/aemcraftserver/aemhungergames/AEMHungerGames.java
+++ src/main/java/net/aemcraftserver/aemhungergames/AEMHungerGames.java
@@ -2,7 +2,9 @@ package main.java.net.aemcraftserver.aemhungergames;
 
 import java.util.logging.Logger;
 
+import main.java.net.aemcraftserver.aemhungergames.commands.CreateRegionCommand;
 import main.java.net.aemcraftserver.aemhungergames.commands.JoinCommand;
+import main.java.net.aemcraftserver.aemhungergames.listeners.BlockListener;
 import main.java.net.aemcraftserver.aemhungergames.util.GameStatus;
 import main.java.net.aemcraftserver.aemhungergames.util.RegionConfig;
 import main.java.net.aemcraftserver.aemhungergames.util.SQLConnection;
@@ -21,7 +23,9 @@ public class AEMHungerGames extends JavaPlugin{
 	public void onEnable(){
 		connection = new SQLConnection();
 		getServer().getPluginCommand("join").setExecutor(new JoinCommand());
+		getCommand("aemregion").setExecutor(new CreateRegionCommand());
 		RegionConfig.createConfig();
+		getServer().getPluginManager().registerEvents(new BlockListener(this), this);
 		
 		lm("Version "+pdfile.getVersion()+" enabled.");
 	}
